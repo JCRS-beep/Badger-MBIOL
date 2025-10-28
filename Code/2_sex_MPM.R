16cabe89434098c25327c28c802e5999d1bd5d2f
Jay <jay.creese@wadham.ox.ac.uk>
2025-10-28 09:41:35 +0000

    adjusting adult survival value, fixing projection plotting

--- Code/2_sex_MPM.R
+++ Code/2_sex_MPM.R
@@ -5,12 +5,12 @@
 Fmat<- matrix(0, nrow=3, ncol=3)# blank female (F) matrix of zeros to fill
 Fmat[1,3]<- 0.27   # female reproduction
 Fmat[2,1]<- 0.65    # cub to yearling 
-Fmat[3,]<- c(0, 0.65, 0.33)  # yearling to adult, adult survival
+Fmat[3,]<- c(0, 0.65, 0.86)  # yearling to adult, adult survival as(n-1/n)
 
 Mmat<-  matrix(0, nrow=3, ncol=3)   # blank male matrix 
 Mmat[1,3]<- 0.32  #  reproduction
 Mmat[2,1]<- 0.67   # cub to yearling 
-Mmat[3,]<- c(0, 0.67, 0.25)  # yearling to adult, adult survival
+Mmat[3,]<- c(0, 0.67, 0.82)  # yearling to adult, male adult survival lower than females
 
 # combining into 2 sex matrix for stage specific vital rates
 mat2<- matrix(0, nrow=6, ncol=6)
@@ -35,15 +35,17 @@ rownames(project1)<- rownames(mat2)   # rows show sex and stage
 colnames(project1)<- c(0:10)          # year of projection (initial =0)
 project1[,1]<- initial    # first row of blank matrix is initial vector
 
-# loop interactive calculating pop structure - create function
-for(t in 2:(years+1)){      # t representing row, each row a year, first col is initial
+# loop calculating pop structure - (later as function mat.proj)
+for(t in 2:(years+1)){      # t representing row, each row a year. first col is initial so starts from 2
   project1[,t]<- mat2 %*% project1[,t-1]    # fill rows of blank projection matrix by multiplying prev col by vital rate matrix
 }
-project1
+project1  
+
 
 # plotting this 10 year projection and colour coding life stages and sex
 cols<- c("red", "red", "red", "blue", "blue", "blue")   # custom colour vector
-matplot(project1, type="l",  # type l for line
+matplot(t(project1),   # transposing matrix- swaps cols for rows
+        type="l",  # type l for line
         col=cols, lty= 1:3,           # red female, blue male, line type
         ylab= "abundance", xlab="time (t)")
 legend("topright", legend= c("Cub", "Yearling", "Adult"),
@@ -53,9 +55,9 @@ legend("topright", legend= c("Cub", "Yearling", "Adult"),
 
 library(popdemo)
 eigs(mat2)
-# lambda= 0.6234685 
-# ss 0.2297954 0.2395743 0.5306303 0.0000000 0.0000000 0.0000000
-# rv 1.0550894 1.0120231 0.9707146 0.0000000 0.0000000 0.000000
+# lambda 1.002837
+# ss 0.0000000 0.0000000 0.0000000 0.2082479 0.1391314 0.6526206
+# rv 0.0000000 0.0000000 0.0000000 0.5323178 0.7967578 1.1925640
 
 #manually calculating pop growth rate
 popN<- apply(project1, 2, sum)   # apply sum func over matrix array (2= by col). Pop size per year
@@ -67,12 +69,18 @@ lambda  # calculates growth rate for each year
 
 
 # stage distribution - incorrect?
-propStage<- project1/popN    # divide each value by total pop size to get proportion each year
+propStage<- project1/popN # divide each value by total pop size to get proportion each year
 matplot(1:11, propStage, type= "l")
 
-# function to calculate stage distribution per year
-propStage<- function(projected, popN)     # inputs= projected matrix, calculated pop size for each year
-    {
-   for(i in 1:ncol(projected))  # repeat for each column
-   
-    }
+# elasticity and sensitivity analysis
+popdemo:: elas(mat2)
+#             Cub(f) Yearling(f) Adult(f)    Cub(m) Yearling(m)  Adult(m)
+# Cub(f)           0           0        0 0.0000000   0.0000000 0.0000000
+# Yearling(f)      0           0        0 0.0000000   0.0000000 0.0000000
+# Adult(f)         0           0        0 0.0000000   0.0000000 0.0000000
+# Cub(m)           0           0        0 0.0000000   0.0000000 0.1108541
+# Yearling(m)      0           0        0 0.1108541   0.0000000 0.0000000
+# Adult(m)         0           0        0 0.0000000   0.1108541 0.6674378
+
+# How to do sensitivity analysis
+ 
