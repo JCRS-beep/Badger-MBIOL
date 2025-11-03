fa362a4955c13668cef59ce2f7444fb83de4ab20
Jay <jay.creese@wadham.ox.ac.uk>
2025-11-03 11:09:58 +0000

    Creating a function to create my matrix (single or 2 sex) using only vital rate inputs

--- Code/Matrix_creation_function.R
+++ Code/Matrix_creation_function.R
@@ -12,38 +12,38 @@ create.matrix<- function(names,   # vector of stage names
                         {         
   nStages<- length(names)                            # number of stages
   mat<- matrix(0, nrow=(nSex*nStages), ncol=(nSex*nStages))     # empty matrix with equal rows and columns as lifestages
-  colnames(mat)<- names                                         # naming columns after stages
-  rownames(mat)<- names                                         # naming rows after stages
+  colnames(mat)<- rep(names, times= nSex)                                        # naming columns after stages
+  rownames(mat)<-  rep(names, times= nSex)                                # naming rows after stages
 
   if(length(r1)==1){    # if only 1 reproductive stage
     mat[1,nStages]<- r1[1]
-     
   }
   else{
     mat[1,(nStages-1)]<-  r1[1]   # otherwise assume final stage sexond value forreproduction 
     mat[1, nStages]<- r1[2]
   }
        
-    for(n in seq_len(nStages-1))                        # loop for as many lifestages before final (growth)
+    for(n in 1:nStages-1)                        # loop for as many lifestages before final (growth)
     {         
   mat[n+1, n]<- g1[n]   # inputs growth rates into respective matrix location
   }   
   mat[(nStages),(nStages)]<- S[1]                      # final stage remaining is adult survival
   
   # this section only runs for 2 sex matrix
-   if(nSex==2){   # only runs if 2 sex model
-         for(m in (nStages+1):(2*(nStages)+1))                   # loop for m= 5-9
-  {         
-    mat[m, m-1]<- g2[m-1]  # inputs growth rates into respective matrix location  
-  }
-     } 
-  if(nSex==2) 
-    { 
+   if(nSex==2){   
+     for (m in seq_len(nStages - 1)) {
+       mat[nStages + (m + 1), nStages + m] <- g2[m]
+     }
+ 
+     if(length(r2)==1){
+       mat[(nStages+1), (2*nStages)]<- r2    # 2nd sex reproductive rate
+     }
+     else{
+       mat[nStages+1,(2*nStages-1)]<-  r2[1]   # otherwise assume final stage sexond value forreproduction 
+       mat[nStages+1, (2*nStages)]<- r2[2]
+     }
      mat[2*nStages, 2*nStages]<- S[2]
-  }
-  if(nSex==2){
-    mat[(nStages+1), 2*nStages]<- r2[2]    # 2nd sex reproductive rate
-  }
+   }
   return(mat)  } 
 
 
@@ -58,7 +58,16 @@ create.matrix<- function(names,   # vector of stage names
 # S= Adult survival vector
 # Output = mat, a matrix of vital rates for each lifestage
 
-# trial
+
+# NOTES----
+# Assumes reproductive phases are the oldest stages, and that all others are immature
+# Assumes equal number of stages in both sexes if 2 sexes included
+# WORKS UP TO 2 REPRODUCTIVE STAGES
+
+
+
+
+# trial----
 stageNames<- c("Cub", "Yearling", "Adult", "Elder")
 mat.test<- create.matrix(names= stageNames,
                          nSex=1,
@@ -71,12 +80,9 @@ mat.test    # SUCCESS
 mat2.test<- create.matrix(names= stageNames,
                           nSex=2,
                           r1=c(0.48, 0.35), 
-                          r2=c(0.6, 0.58),
-                          g1=c(0.22, 0.3, 0.6), 
+                          r2=c(0.6, 0.58),    # UNUSED
+                          g1=c(0.29, 0.49, 0.81),
+                          g2= c(0.33, 0.36, 0.64),
                           S=c(0.2, 0.1))
-mat2.test
+mat2.test  # SUCCESS!! :D
 
-# NOTES----
-# Assumes reproductive phases are the oldest stages, and that all others are immature
-# Assumes equal number of stages in both sexes if 2 sexes included
-# WORKS UP TO 2 REPRODUCTIVE STAGES
