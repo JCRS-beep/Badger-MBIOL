fe6076574fe637cf878ae1271cefe49c96c23e05
Jay <jay.creese@wadham.ox.ac.uk>
2025-11-17 13:10:38 +0000

    Improving spacing of my functions

--- Code/Functions/Mating_systems_function.R
+++ Code/Functions/Mating_systems_function.R
@@ -2,25 +2,25 @@
 # 16/11/2025
 
 
-Mating.Fmat<- function(params,     # density dependent parameters
+Mating.Fmat <- function(params,     # density dependent parameters
                        stagenames,   # Stages in life cycle graph (single sex)
                        Nf,        # Adult and yearling females
                        Nm)        # Adult and yearling males
 {
   if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
-  nStages<- length(stagenames) 
+  nStages <- length(stagenames) 
   
-  Fmat<-matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
-  rownames(Fmat)<- stagenames
-  colnames(Fmat)<- stagenames
+  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
+  rownames(Fmat) <- stagenames
+  colnames(Fmat) <- stagenames
   
-  K<- params$rep_K
-  h<- params$h
-  f<- (K*Nm)/(Nm+Nf*h^-1)    # fertility function
-  Fmat[1,nStages/2]<-0.5*f      # female cub production by females
-  Fmat[(nStages/2)+1, nStages/2]<- 0.5*f   # male cub production by females
+  K <- params$rep_K
+  h <- params$h
+  f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function
+  Fmat[1,nStages/2] <- 0.5*f      # female cub production by females
+  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f   # male cub production by females
  
-   return(Fmat)
+   return(Fmat) 
 }
 # Function name= Mating.Fmat
 # Inputs:
--- Code/Functions/Ricker_apply_function.R
+++ Code/Functions/Ricker_apply_function.R
@@ -2,8 +2,8 @@
 # 16/11/2025
 
 
-ricker<- function(params, N){   # Using params (b) and population size input
-  dd_fun<-exp(-params$b*N)
+ricker <- function(params, N){   # Using params (b) and population size input
+  dd_fun <- exp(-params$b*N)
   return(dd_fun)      # returns value of multiplier
 }
 
@@ -16,40 +16,40 @@ ricker<- function(params, N){   # Using params (b) and population size input
 
 
 # Applying ricker function density dependence to matrix
-apply.DD<- function(params, Fmat, Umat, N, DDapply="matrix")   # apply ricker to whole matrix, survival or fertility
+apply.DD <- function(params, Fmat, Umat, N, DDapply="matrix")   # apply ricker to whole matrix, survival or fertility
 { 
   # making sure mats match 
   if (sum(dim(Fmat)==dim(Umat))!=2){
     stop("Reproductive (Fmat) and survival (Umat) matrices 
          must have the same dimensions.")
   }
-  rick<-ricker(params, N)    # ricker function embedded
-  Amat<- Fmat + Umat
+  rick <- ricker(params, N)    # ricker function embedded
+  Amat <- Fmat + Umat
   
   # applying based on method
   if(DDapply %in% c("matrix", "Matrix"))  {
-    Amat_N<- Amat*rick  
+    Amat_N <- Amat*rick  
   
  } else if(DDapply %in% c("Survival", "survival", "Umat")){
-  Umat_N<- Umat*rick
-  Amat_N<- Fmat + Umat_N
+  Umat_N <- Umat*rick
+  Amat_N <- Fmat + Umat_N
   
  } else if(DDapply  %in% c("Ferility", "fertility", "Fmat")) {
-  Fmat_N<- Fmat*rick
-  Amat_N<- Fmat_N + Umat
+  Fmat_N <- Fmat*rick
+  Amat_N <- Fmat_N + Umat
   
  } else if(DDapply %in% c("Recruitment", "recruitment")){
    # working out positions of cub in matrix
-   nStages<- nrow(Umat)/2  
+   nStages <- nrow(Umat)/2  
   # Mcub<- c(nStages+2, nStages+1)         # assumes female cubs are [1,], male cubs [nStages+1, ] 
-   Umat_N<- Umat
-   Umat_N[2,1]<- Umat[2,1]* rick # female cub survival to yearlings
-   Umat_N[(nStages+2), (nStages+1)]<- Umat_N[(nStages+2), (nStages+1)]* rick # male cub survival to yearlings 
-   Amat_N<- Fmat + Umat_N
+   Umat_N <- Umat
+   Umat_N[2,1] <- Umat[2,1]* rick # female cub survival to yearlings
+   Umat_N[(nStages+2), (nStages+1)] <- Umat_N[(nStages+2), (nStages+1)]* rick # male cub survival to yearlings 
+   Amat_N <- Fmat + Umat_N
   
    # Applying to Fmat
-   Fmat_N<- Fmat*rick
-   Amat_N<- Fmat_N + Umat_N
+   Fmat_N <- Fmat*rick
+   Amat_N <- Fmat_N + Umat_N
  } 
 
   return(Amat_N) 
@@ -65,6 +65,3 @@ apply.DD<- function(params, Fmat, Umat, N, DDapply="matrix")   # apply ricker to
 # Use:  Creates a density dependent Amat depending on DDapplication to existing matrices, Uma and Fmat
 
 # issues in application- recruitment subsetting for male survival incorrect?
-
-# Testing application function
-apply.DD<- function(params, Fmat, Umat, N, DDapply="matrix") 
