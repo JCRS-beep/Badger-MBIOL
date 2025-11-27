666381bd87e8b997be5666d02bdbbefa2c0e3e7f
Jay <jay.creese@wadham.ox.ac.uk>
2025-11-27 17:03:29 +0000

    adding different mating function types, correcting to calculate just maximum number of possible pairs instead of incorporating into Fmat

--- Code/Functions/Mating_systems_function.R
+++ Code/Functions/Mating_systems_function.R
@@ -2,35 +2,64 @@
 # 16/11/2025
 
 
-Mating.Fmat <- function(params,     # density dependent parameters
-                       stagenames,   # Stages in life cycle graph (single sex)
+mating.func <- function(params,     # density dependent parameters
+                    #   stagenames,   # Stages in life cycle graph (single sex)
                        Nf,        # Adult and yearling females
-                       Nm)        # Adult and yearling males
+                       Nm,             # Adult and yearling males
+                     Mfunction= "HM")        # mating function applied
 {
-  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
-  nStages <- length(stagenames) 
-  
-  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
-  rownames(Fmat) <- stagenames
-  colnames(Fmat) <- stagenames
+#  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
+#  nStages <- length(stagenames) 
+  #
+#  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
+#  rownames(Fmat) <- stagenames
+#  colnames(Fmat) <- stagenames
   
   K <- params$rep_K
   h <- params$h
-  f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function
-  Fmat[1,nStages/2] <- 0.5*f      # female cub production by females
-  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f   # male cub production by females
- 
-   return(Fmat) 
+  Sf<- params$Sc_f_max
+  Sm<- params$Sc_m_max 
+  
+  #Harmonic mean mating function
+  if(Mfunction %in% c( "HM", "HMMF", "Harmonic Mean Mating Function", "harmonic mean mating function", "Harmonic Mean", "harmonic mean")){
+  U <- (2* Nf * Nm*h)/ (Nf + Nm* h) # females in unions given pop structure
+  
+ #   f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function= cubs produced per adult female - Necessary?
+  
+  # Minimum mating function
+  } else if(MFunction %in% c("Minimum Mating Function", "minimum mating function", "Minimum", "minimum", "min", "Min")){
+   # defining number of pairs formed (U)
+    if(Nf < Nm*h){
+    U <- Nf
+   } else if(Nm*h < Nf){
+     U <- Nm*h
+   } #fertility coefficient
+#    f<- (K*U)/ Nf  # f= cubs produced by adult female
+     
+     # Mod Harmonic mean mating function?
+  } else if(MFunction %in% c("Modified Hamonic Mean Mating Function", "modified harmonic mean mating function", "ModHarmonic", "modharmonic"))   {
+    if(Nf < (2* Nf*Nm*h) / (Nf +Nm*h)) {
+      U <- Nf
+    } else if((2* Nf*Nm*h) / (Nf +Nm*h) < Nf){
+      U <- (2* Nf*Nm*h) / (Nf +Nm*h)
+    }
+  }
+#  Fmat[1,nStages/2] <- 0.5*f * Sf    # female cub production by females
+#  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f* Sm  # male cub production by females
+
+  # incorporating in projection- using U instead of Nf in population size
+
+   return(U) 
 }
-# Function name= Mating.Fmat
+# Function name= Mating.func
 # Inputs:
 # params: density dependent parameters, including k (litter size) & h (harem size)
 # stagenames Stages in life cycle , where length()= rows of matrix
-# Nf: Number of Adult and yearling females in population
-# Nm:Number of Adult and yearling males
+# Nf: Number of Adult females in population
+# Nm:Number of Adult males
+# Mfunc : Mating function, can be Harmonic mean, minimum or mod harmonic mean
 # 
-# Use: Creates a fertility matrix of expected cub production per female for male and female cubs. Ignores male reproduction aside from mating contribution
-# f= female fertility function (number of cubs per female given Nf females and Nm males in pop)
-# 0.5 assumes sex ratio is equal and density independent, so half the cubs produced are female
-
+# Use: number of pairs formed based on chosen mating function
+# Returns maximum possible number of pairs formed in pop based on male and female abundance. Cavveats- h may increase with increasing density, then plateau
+# For use in matrix projection- use U as Nf instead of vector input
 
--- Code/Mating_systems.R
+++ Code/Mating_systems.R
@@ -4,21 +4,20 @@
 # Incorporating mating systems 
 
 # Seperating Fmat and Umat
-stages<- c("Cub_f", "Yearling_f", "Adult_f", "Cub_m", "Yearling_m", "Adult_m")
-mat.blank<- matrix(0, ncol=6, nrow=6)
+stages<- c( "Yearling_f", "Adult_f", "Yearling_m", "Adult_m")
+mat.blank<- matrix(0, ncol=4, nrow=4)
 rownames(mat.blank)<- stages
 colnames(mat.blank)<- stages
 
 ex_Fmat<- mat.blank   # copying to visualise Fertility matrix (not female mat)
 # Assumes females only reproduction
-ex_Fmat[1,3]<- "0.5f"      # where f= litter size per *adult* female for given number males and females (Nm, Nf)
+ex_Fmat[1,3]<- "0.5f"      # where f= litter size per female for N males and females (Nm, Nf), multiplied cub survival
 ex_Fmat[4,3]<-  "0.5f"
 
 
 # Inspo from ex functions
 params<- data.frame(      # dataframe with density dependent parameters
-  Ffmax= 1.6,     # F fecundity max (max cubs per adult female)
-  Fmmax= 2.1,     # Male fecundity  (max cubs per adult female)
+  fmax= 3*0.65,     # fecundity max (max cubs per adult female=3, 0.65= max cub survival)
   Sc_f_max=0.65,   # 
   Sc_m_max=0.65, 
   b=0.02,
@@ -26,85 +25,58 @@ params<- data.frame(      # dataframe with density dependent parameters
   h= 6   # harem size per male
   )
 
-
-# creating Fmat with mating systems
-Mating.Fmat<- function(params,     # density dependent parameters
-                       stagenames,   # Stages in life cycle graph (single sex)
-                       Nf,        # Adult and yearling females
-                       Nm)        # Adult and yearling males
-  {
-  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
-   nStages<- length(stagenames) 
-     
-  Fmat<-matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
-  rownames(Fmat)<- stagenames
-  colnames(Fmat)<- stagenames
+# Creating mating function.  Uses number of pairs for max number of possible unions (if no opposite sex, no mating) -----
+mating.func <- function(params,     # density dependent parameters
+                        #   stagenames,   # Stages in life cycle graph (single sex)
+                        Nf,        # Adult and yearling females
+                        Nm,             # Adult and yearling males
+                        Mfunction= "HM")        # mating function applied
+{
+  #  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
+  #  nStages <- length(stagenames) 
+  #
+  #  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
+  #  rownames(Fmat) <- stagenames
+  #  colnames(Fmat) <- stagenames
   
-  K<- params$rep_K
-  h<- params$h
-  f<- (K*Nm)/(Nm+Nf*h^-1)    # fertility function
-  Fmat[1,nStages/2]<-0.5*f      # female cub production by females
-  Fmat[(nStages/2+1),nStages/2]<- 0.5*f   # male cub production by females
-  return(Fmat)
-  }
-
-# Function name= Mating.Fmat
-# Calculates resulting matrix (Fmat) using harmonic fertility function  
-
-Fmat<- Mating.Fmat(params=params, stagenames=stages, Nf=20, Nm=18)  # starting pop = 20 fems, 18 males
-Fmat   # SUCCESS!
-
-
-
-#Can't project without Amat= Fmat + Umat, otherwise extinction ---- 
-Fmat.proj<-function(initial, params, stagenames, time, return.vec= FALSE) {
- 
-   out<- list(pop=vector(), vec=matrix(), mat=matrix())  # objects returned
- 
-   # Time as integer
-  time <- as.integer(time)
-  # Initial vector:
-  dims<-length(stagenames)      # dims is nrow and ncol of Fmat 
-  if (length(initial) != dims) stop("initial pop vector must equal length(stagenames)")
-  n0 <- as.numeric(initial)     # no intial pop vector
-   
+  K <- params$rep_K
+  h <- params$h
+  Sf<- params$Sc_f_max
+  Sm<- params$Sc_m_max 
   
-#   if (is.null(memberN)){
-#     memberN<- 1:length(stagenames)   # all members contribute to pop size
-#   }
-   Nf<- n0[(dims/2)-1] + n0[(dims/2)]      # extract Nf and Nm from n0. adult and yearlings included, only cubs excluded
-   Nm<- n0[dims]
-   
-   # Initialize the output for population vector:
-   Vec <- matrix(0, ncol = dims, nrow = time + 1)  # matrix to fill with stage abundance for each time step (each row)
-   Pop <- rep(NA, (time + 1))       # vector to fill with total pop size each year
-   colnames(Vec) <- stagenames   # naming cols matrix as stages 
-   rownames(Vec) <- 0:(time)   # rows correspond to each year of projection
-    Vec[1, ] <- n0                   
-    Pop[1] <- sum(n0)    
- 
-     for (i in 1:time) {  
-     thisNf<- Vec[i,dims/2]    # Nf is mid col in Vec matrix
-     thisNm<- Vec[i,dims]        # assumes adult males final entry in matrix
+  #Harmonic mean mating function
+  if(Mfunction %in% c( "HM", "HMMF", "Harmonic Mean Mating Function", "harmonic mean mating function", "Harmonic Mean", "harmonic mean")){
+    U <- (2* Nf * Nm*h)/ (Nf + Nm* h) # females in unions given pop structure
     
-     thisFmat<-Mating.Fmat(params, dims,thisNf, thisNm)   # applies Fmat creation to 
-     Vec[(i + 1), ] <- thisFmat %*% Vec[i, ]    # mat multiplication of Fmat rates and prev year stage abundance gives new stage abundance
-     Pop[i + 1] <- sum(Vec[(i + 1), ])        # new total pop size sums stage abundance
-   }
-   
-  # out objects
-   dim(Pop) <- time + 1
-   out$pop <- Pop
-   if (isTRUE(return.vec)) {
-     out$vec <- Vec        # issues in this line?
-  } 
-   out$mat <- thisFmat
-   
-   return(out)
-     }
+    #   f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function= cubs produced per adult female - Necessary?
+    
+    # Minimum mating function
+  } else if(Mfunction %in% c("Minimum Mating Function", "minimum mating function", "Minimum", "minimum", "min", "Min")){
+    # defining number of pairs formed (U)
+    if(Nf < Nm*h){
+      U <- Nf
+    } else if(Nm*h < Nf){
+      U <- Nm*h
+    } #fertility coefficient
+    #    f<- (K*U)/ Nf  # f= cubs produced by adult female
+    
+    # Mod Harmonic mean mating function?
+  } else if(Mfunction %in% c("Modified Hamonic Mean Mating Function", "modified harmonic mean mating function", "ModHarmonic", "modharmonic"))   {
+    if(Nf < (2* Nf*Nm*h) / (Nf +Nm*h)) {
+      U <- Nf
+    } else if((2* Nf*Nm*h) / (Nf +Nm*h) < Nf){
+      U <- (2* Nf*Nm*h) / (Nf +Nm*h)
+    }
+  }
+  #  Fmat[1,nStages/2] <- 0.5*f * Sf    # female cub production by females
+  #  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f* Sm  # male cub production by females
+  
+  # incorporating in projection- using U instead of Nf in population size
+  
+  return(U) 
+}
 
-# Testing function----
-initial=c(10,10,10,10,10,10)  # equal all sexes and ages
-proj.test<- Fmat.proj(initial, params, stagenames= stages, time=10, return.vec= TRUE)
-# 'Error in thisFmat %*% Vec[i, ] : non-conformable arguments'   
 
+# Testing function ----
+pairs <- mating.func(params=params, Nf=10, Nm=8, Mfunction= "Min")  # starting pop = 10 fems, 8 males
+pairs   # SUCCESS
