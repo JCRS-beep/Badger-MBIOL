21a732a71ab008b86a8f1ad80feb6abd175b70fd
Jay <jay.creese@wadham.ox.ac.uk>
2025-12-03 16:43:01 +0000

    Code now runs using mating system function to create Fmat.

--- Code/Density_dependence.R
+++ Code/Density_dependence.R
@@ -15,17 +15,18 @@ ex_Umat[4,3]<- "G2m"
 ex_Umat[4,4]<- "Sm"
 
 Umat<-mat.blank  # creating my working matrix. Values from 
-Umat[2,1]<- 0.67  
-Umat[2,2]<- 0.78
-Umat[3,3]<- 0.65
+Umat[2,1]<- 0.67   # yearling f survival
+Umat[2,2]<- 0.78   # adult f survival
+Umat[4,3]<- 0.65   # yearling m survival
 Umat[4,4]<- 0.72
 
 # loading Parameters for density and Fmat creation 
 params<- data.frame(      # dataframe 
-  fmax= 3.2*0.65,     # F fecundity max (max cubs per adult female) * cub survival
-  Sc_max=0.65,   # max cub survival (equal for sexes)
+  fmax= 3.2,     # F fecundity max (max cubs per adult female) 
+  Sc_f_max=0.65,   # max cub survival (equal for sexes)
+  Sc_m_max=0.65,
   b=0.002,       # temp value- must be calculated from provided datasets
-  rep_K= 3,          #litter size (K)
+  rep_K= 4,          #litter size (K)
   h= 6   # harem size per male
 )
 
@@ -39,36 +40,40 @@ ex_Umat[3,2]<- "0.5*e^-2bN*f"
 # creating Fmat basic, with max reproductive output
 Fmat<- mat.blank # Fmat will be blank, each year of projection reproductive rates are calculated
 f<- params$fmax
-Sf<- params$Sc_max
-Sm<- params$Sc_max
+Sf<- params$Sc_f_max
+Sm<- params$Sc_m_max
 Fmat[1,2]<- 0.5*f*Sf   # this Fmat has max values of reproduction (freq and density independent)
-Fmat[4,2]<- 0.5*f * Sm
+Fmat[3,2]<- 0.5*f * Sm
 
 # Mating function -----
 # can be loaded from script in Function folder
 mating.func <- function(params,     # density dependent parameters
-                        #   stagenames,   # Stages in life cycle graph (single sex)
+                        stagenames,   # Stages in life cycle graph (single sex)
                         Nf,        # Adult and yearling females
                         Nm,             # Adult and yearling males
-                        Mfunction= "HM")        # mating function applied
+                        Mfunction= "min", 
+                        return.mat= FALSE)        # mating function applied
 {
-  #  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
-  #  nStages <- length(stagenames) 
-  #
-  #  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
-  #  rownames(Fmat) <- stagenames
-  #  colnames(Fmat) <- stagenames
+  if(is.null(stagenames) || length(stagenames)== 0 & return.mat==TRUE) stop("stagenames must be provided for correct matrix dimension calculations")
   
+  # naming objects used
   K <- params$rep_K
   h <- params$h
-  Sf<- params$Sc_f_max
+  Sf<- params$Sc_f_max    # assumes different survival for male and female cubs
   Sm<- params$Sc_m_max 
+  
+  
+  # creating the output obj 
+  out<- list(U=numeric(), Fmat=matrix())
+  U <- NA    # blank
+  Fmat <- matrix(0, ncol= length(stagenames), nrow= length(stagenames)) # blank matrix
+  rownames(Fmat) <- stagenames
+  colnames(Fmat) <- stagenames
   
   #Harmonic mean mating function
   if(Mfunction %in% c( "HM", "HMMF", "Harmonic Mean Mating Function", "harmonic mean mating function", "Harmonic Mean", "harmonic mean")){
-    U <- (2* Nf * Nm*h)/ (Nf + Nm* h) # females in unions given pop structure
-    
-    #   f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function= cubs produced per adult female - Necessary?
+    U <- (2* Nf * Nm*h)/(Nf + Nm* h) # females in unions given pop structure
+    f <- K*Nm/ (Nm + Nf*h^-1)
     
     # Minimum mating function
   } else if(Mfunction %in% c("Minimum Mating Function", "minimum mating function", "Minimum", "minimum", "min", "Min")){
@@ -78,29 +83,27 @@ mating.func <- function(params,     # density dependent parameters
     } else if(Nm*h < Nf){
       U <- Nm*h
     } #fertility coefficient
-    #    f<- (K*U)/ Nf  # f= cubs produced by adult female
+    f<- (K*U)/Nf  # f= cubs produced by adult female
     
     # Mod Harmonic mean mating function?
   } else if(Mfunction %in% c("Modified Hamonic Mean Mating Function", "modified harmonic mean mating function", "ModHarmonic", "modharmonic"))   {
     if(Nf < (2* Nf*Nm*h) / (Nf +Nm*h)) {
       U <- Nf
     } else if((2* Nf*Nm*h) / (Nf +Nm*h) < Nf){
-      U <- (2* Nf*Nm*h) / (Nf +Nm*h)
+      U <- (2* Nf*Nm*h) / (Nf+ Nm)
     }
+    f <- (K*U)/Nf   }
+  
+  if(return.mat==TRUE) {
+    nStages <- length(stagenames)/2 # number stages for each sex
+    Fmat[1,nStages] <- 0.5* f* Sf    # female cub production by females
+    Fmat[nStages+1, nStages] <- 0.5* f* Sm  # male cub production by females
+    out$Fmat <- Fmat
   }
-  #  Fmat[1,nStages/2] <- 0.5*f * Sf    # female cub production by females
-  #  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f* Sm  # male cub production by females
   
-  # incorporating in projection- using U instead of Nf in population size
+  out$U <- U
   
-  return(U) 
-}
- 
-# Ricker function----
-# or load from script in Function folder ----
-ricker<- function(params, N){   # Using params (b) and population size input
-  dd_fun<-exp(-params$b*N)
-  return(dd_fun)      # returns value of multiplier
+  return(out) 
 }
 
 # Matrix Application loaded from ricker script----
@@ -146,10 +149,13 @@ apply.DD <- function(params, Fmat, Umat, N, DDapply="matrix")   # apply ricker t
   return(Amat_N) 
 }
 
+# Creating Fmat for max rates WITH MATING FUNCTIONS
+Fmat2 <- mating.func(params, stages, Nf= 20, Nm= 20, Mfunction= "modharmonic", return.mat=TRUE)
+
 
 
 # Applying to matrix with function applyDD for a static population - 40 adults, 20 females, 20 males -----
-Amat<-apply.DD(params, Fmat, Umat, N=40, DDapply="recruitment") # N includes only adults and yearlings, so that N= Nf + Nm
+Amat<-apply.DD(params, Fmat2$Fmat, Umat, N=40, DDapply="recruitment") # N includes only adults and yearlings, so that N= Nf + Nm
 Amat  
 
 # DD projection function ----
@@ -157,14 +163,40 @@ Amat
 
 # testing projection function----
 initial <- c(10, 10, 10, 10)
-test_proj <-dd.proj( Fmat, 
-                     Umat, 
-                     initial, 
-                     params, 
-                     stagenames = stages, 
-                     time = 20, 
-                     memberN=NULL,  # which individuals contribute to pop size? (as vec)
-                     DDapply= recruitment, 
-                     Mfunction= "Min",
-                     return.vec= FALSE) 
-# SUCCESS!!
+test_proj <-dd.proj(Umat, 
+                    initial, 
+                    params, 
+                    stagenames = stages, 
+                    time = 20, 
+                    memberN=NULL,  # which individuals contribute to pop size? (as vec)
+                    DDapply= "recruitment", 
+                    Mfunction= "min",
+                    return.vec= TRUE) 
+# SUCCESS? 
+
+ 
+# Plotting pop strucutre over time
+library(ggplot2)
+time_vec <- c(0:20)
+(Nplot <- ggplot(NULL, aes(x=time_vec, y=test_proj$pop)) + 
+                           xlab("time(years)") +
+                           ylab("Population size") +
+  geom_point())      # nice plateau, but 300 individuals appears high!
+
+(test_plot_N <-dd_plot(test_proj, 
+                      y_val= "N", 
+                      ylab = "Population size", 
+                      xlab = "time (t)",
+                      col= "black",
+                      legend.pos = "topleft",
+                      cex.legend = 0.8))
+
+
+(test_plot_vec <- dd_plot(test_proj, 
+                    y_val= "Vec", 
+                    ylab = "abundance", 
+                    xlab = "time (t)",
+                    col= c("red", "blue"),
+                    legend.pos = "topleft",
+                    cex.legend = 1))
+# why do yearling m and f have identical projections? Why do adults appear much more abudance than yearlings? 
--- Code/Mating_systems.R
+++ Code/Mating_systems.R
@@ -27,28 +27,32 @@ params<- data.frame(      # dataframe with density dependent parameters
 
 # Creating mating function.  Uses number of pairs for max number of possible unions (if no opposite sex, no mating) -----
 mating.func <- function(params,     # density dependent parameters
-                        #   stagenames,   # Stages in life cycle graph (single sex)
+                        stagenames,   # Stages in life cycle graph (single sex)
                         Nf,        # Adult and yearling females
                         Nm,             # Adult and yearling males
-                        Mfunction= "HM")        # mating function applied
+                        Mfunction= "min", 
+                        return.mat= FALSE)        # mating function applied
 {
-  #  if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimension calculations")
-  #  nStages <- length(stagenames) 
-  #
-  #  Fmat <- matrix(0, nrow=(nStages), ncol= (nStages))   # blank matrix with dim of stages
-  #  rownames(Fmat) <- stagenames
-  #  colnames(Fmat) <- stagenames
+  if(is.null(stagenames) || length(stagenames)== 0 & return.mat==TRUE) stop("stagenames must be provided for correct matrix dimension calculations")
   
+  # naming objects used
   K <- params$rep_K
   h <- params$h
-  Sf<- params$Sc_f_max
+  Sf<- params$Sc_f_max    # assumes different survival for male and female cubs
   Sm<- params$Sc_m_max 
+  
+  
+  # creating the output obj 
+  out<- list(U=numeric(), Fmat=matrix())
+  U <- NA    # blank
+  Fmat <- matrix(0, ncol= length(stagenames), nrow= length(stagenames)) # blank matrix
+  rownames(Fmat) <- stagenames
+  colnames(Fmat) <- stagenames
   
   #Harmonic mean mating function
   if(Mfunction %in% c( "HM", "HMMF", "Harmonic Mean Mating Function", "harmonic mean mating function", "Harmonic Mean", "harmonic mean")){
     U <- (2* Nf * Nm*h)/ (Nf + Nm* h) # females in unions given pop structure
-    
-    #   f <- (K*Nm)/(Nm+Nf*h^-1)    # fertility function= cubs produced per adult female - Necessary?
+    f <- K*Nm/ (Nm + Nf*h^-1)
     
     # Minimum mating function
   } else if(Mfunction %in% c("Minimum Mating Function", "minimum mating function", "Minimum", "minimum", "min", "Min")){
@@ -58,25 +62,35 @@ mating.func <- function(params,     # density dependent parameters
     } else if(Nm*h < Nf){
       U <- Nm*h
     } #fertility coefficient
-    #    f<- (K*U)/ Nf  # f= cubs produced by adult female
+    f<- (K*U)/ (Nf+ Nm)  # f= cubs produced by adult female
     
     # Mod Harmonic mean mating function?
   } else if(Mfunction %in% c("Modified Hamonic Mean Mating Function", "modified harmonic mean mating function", "ModHarmonic", "modharmonic"))   {
     if(Nf < (2* Nf*Nm*h) / (Nf +Nm*h)) {
       U <- Nf
     } else if((2* Nf*Nm*h) / (Nf +Nm*h) < Nf){
-      U <- (2* Nf*Nm*h) / (Nf +Nm*h)
+      U <- (2* Nf*Nm*h) / (Nf+ Nm)
     }
+    f <- (K*U)/ (Nf+ Nm)}
+  
+  if(return.mat==TRUE) {
+    nStages <- length(stagenames)/2 # number stages for each sex
+    Fmat[1,nStages] <- 0.5* f* Sf    # female cub production by females
+    Fmat[nStages+1, nStages] <- 0.5* f* Sm  # male cub production by females
+    out$Fmat <- Fmat
   }
-  #  Fmat[1,nStages/2] <- 0.5*f * Sf    # female cub production by females
-  #  Fmat[(nStages/2)+1, nStages/2] <- 0.5*f* Sm  # male cub production by females
   
-  # incorporating in projection- using U instead of Nf in population size
+  out$U <- U
   
-  return(U) 
+  return(out) 
 }
 
 
 # Testing function ----
-pairs <- mating.func(params=params, Nf=10, Nm=8, Mfunction= "Min")  # starting pop = 10 fems, 8 males
+pairs <- mating.func(params,     # density dependent parameters
+                     stagenames= stages,   # Stages in life cycle graph (single sex)
+                     Nf=25,        # Adult and yearling females
+                     Nm=12,             # Adult and yearling males
+                     Mfunction= "min", 
+                     return.mat=TRUE)        # mating function applied)  # starting pop = 10 fems, 8 males
 pairs   # SUCCESS
