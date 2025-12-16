bcd7b5c8924e8847003dee40add421a5a8e1b024
Jay <jay.creese@wadham.ox.ac.uk>
2025-12-16 11:29:09 +0000

    Creating fig extraction folder within data, digitising entries to extract as dataframe. Info no vital rates, population size over time in different populations

--- Code/parameter_estimation.R
+++ Code/parameter_estimation.R
@@ -1,21 +1,12 @@
 # Calculating parameters for density dependence
-# 11.11.2025
+# 13.12.2025
 
+# installing digitise package
+# install.packages("devtools")
+# devtools::install_github("daniel1noble/metaDigitise")
+library(metaDigitise)
 
-# load the data
-DD_data<- read.csv("Data/DD_vital_rates.csv")   # data from Tuyttens 2000
-head(DD_data)
+# manually digitising all graphs
+figs <- metaDigitise(dir ="C:/Users/jaycr/OneDrive - Nexus365/UNI/Masters/Project resources/Badger-MBIOL/Data/FigExtraction")
 
-
-byear<- plot(x=DD_data$Year, y= DD_data$Births_f) # changes in births per year- link? 
-
-
-# linking cub production AND SURVIVAL to population density 
-bd_plot<- plot(x= DD_data$Density, y=DD_data$Births_f)    # births against density- no clear pattern seen
-
-# loading ggplot for nicer graphs
-library(ggplot2)
-
-(birth_plot<- ggplot(data= DD_data, aes(x=Density, y=Births_f, colour= Site))+  # seeing if site explains some variation
-  geom_point())     
-# 
+figs
