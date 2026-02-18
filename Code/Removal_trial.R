83779195e4025e3a1c44df5626ce2a0fa3443672
Jay <jay.creese@wadham.ox.ac.uk>
2026-02-18 13:52:03 +0000

    creating a biased removal option for sex, age or specified entries of lifestages

--- Code/Removal_trial.R
+++ Code/Removal_trial.R
@@ -27,7 +27,7 @@ params<- data.frame(fmax= 0.8436,   # F fecundity max (max cubs per adult female
 
 # reaches plateau around 20 years - removal at year = 25?
 
-#removal funcs similar to DDproj - replacement? Need to stop simulation once pop reaches 0 - in some cases becomes negative ----
+#removal funcs similar to DDproj - WHAT ABOUT BIASED REMOVALS?
 rem.proj <- function(Umat,   # MAX SURVIVAL
                      initial, 
                      params, 
@@ -38,6 +38,8 @@ rem.proj <- function(Umat,   # MAX SURVIVAL
                      Mfunction= "Min",
                      intensity= NULL,  # percentage you want REMOVED from pop at time T=ry
                      remyear = NULL,  # removal year = decrease from following year
+                     rem_strat = "random",  # if specified removals, "adults, females, yearlings, males, yearling females, 
+                     bias = NULL , # strength of bias as percentage (range??)
                      return.vec= TRUE, 
                      return.remvec = FALSE) 
 {
@@ -48,12 +50,11 @@ rem.proj <- function(Umat,   # MAX SURVIVAL
   if (is.null(initial)) stop("You must provide initial population vector with each stage abundance")
   else(n0 <- as.numeric(initial))
   
-  
   if (length(initial) != length(stagenames)) stop("initial pop vector must equal length(stagenames)")
   if(is.null(stagenames) || length(stagenames)== 0) stop("stagenames must be provided for correct matrix dimensions")
   
   if (is.null(params)) stop("You must provide parameters for selected density-dependent function")
-  
+  if (rem_strat != "random" && 1+bias > 1/(intensity/100)) stop("Bias value provided leads to p(removal) >1. Re-enter below", 1/intensity, "and repeat")
   
   nStages <- length(stagenames)/2      # how many stages
   
@@ -138,10 +139,57 @@ rem.proj <- function(Umat,   # MAX SURVIVAL
   
   # pop removal
   # ------------------------------------------------
-  if (is.numeric(intensity)){
-    #generating the distribution
+  if (is.numeric(intensity)) {
+    if(rem_strat == "random"){
+  if (is.null(bias) == FALSE) paste("ignoring bias value since removal is random across ages and sexes")
+    #generating the distribution - varies with rem strat
     prop <- rnorm(length(stagenames), mean = intensity/100, sd= intensity/1000)  # 4 samples from dist mean 0.5, sd 0.05
-    rem_vec <- Vec[ry+1,] * prop    # remaining <- Vec[ry+1] - rem_vec
+ 
+  } else if (is.numeric(intensity) && rem_strat %in% c("adults", "Adults", "adult", "Adult", "yearlings", "Yearlings", "yearling", "Yearlings")){   # want to specify age and sex prob  - adult male, yearling fem.. 
+    if (is.null(bias) == TRUE) stop("please provide strength of bias as value 0-")
+    if (rem_strat %in% c("adults", "Adults", "adult", "Adult")){
+    y_rem <- rnorm(nStages, mean = ((1-bias)*intensity)/100, sd = intensity/1000) 
+    a_rem <- rnorm(nStages, mean = ((1+bias)*intensity)/100, sd = intensity/1000)    
+    }
+    else if (rem_strat %in% c("yearlings", "Yearlings", "yearling", "Yearlings")){
+      y_rem <- rnorm(nStages, mean = ((1+bias)*intensity)/100, sd = intensity/1000) 
+      a_rem <- rnorm(nStages, mean = ((1-bias)*intensity)/100, sd = intensity/1000) 
+    }
+    # col binding so each row represents stage
+    bind <- cbind(y_rem, a_rem)
+    prop <- c(bind[1,], bind[2,])   # 4 proportions to remove in correct order (yf, af, ym, am)
+ 
+  } else if (is.numeric(intensity) && rem_strat %in% c("females", "Females", "female", "Female", "males", "Males", "male", "Male")){  
+    if (is.null(bias) == TRUE) stop("please provide strength of bias as value 0-")
+    if(rem_strat %in% c("females", "Females", "female", "Female")){
+     f_rem <- rnorm(nStages, mean = ((1+bias)*intensity)/100, sd = intensity/1000) # bias applied to females
+     m_rem <- rnorm(nStages, mean = ((1-bias)*intensity)/100, sd = intensity/1000) 
+  
+       } else if(rem_strat %in% c("males", "Males", "male", "Male")){
+         f_rem <- rnorm(nStages, mean = ((1-bias)*intensity)/100, sd = intensity/1000) # bias applied to females
+         m_rem <- rnorm(nStages, mean = ((1+bias)*intensity)/100, sd = intensity/1000) 
+       }
+    bind <- cbind(f_rem, m_rem)
+    prop <- c(bind[,1], bind[,2])   # 4 proportions to remove in correct order (yf, af, ym, am)
+ 
+  } else if (is.numeric(intensity) && is.numeric(rem_strat)){
+    if (is.null(bias) == TRUE) stop("please provide strength of bias as value 0-")
+    # how to specify is specific element biased?  numeric vector or integer 1:4, then apply bias to element
+    # ex rem strat = 3 (ym)
+    bi <- length(rem_strat)  # how many elements provided
+
+    rem <- rnorm((1- bi), mean = ((1+bias)*intensity)/100, sd = intensity/1000)   # unbiased, 1- number of biased samples needed
+    rembi <- rnorm(bi, mean = ((1+bias)*intensity)/100, sd = intensity/1000)
+    
+    # how to order? biased p pos matched to bias stage? 
+    prop <- rep(NA, length(stagenames))
+    prop[rem_strat] <- rembi
+    prop[-rem_strat] <- rem    # all non biased stages, add unbiased probs
+      
+    }
+
+    
+  rem_vec <- Vec[ry+1,] * prop    # rem vec is how many individuals we remove from the population 
     
     Vec[ry + 2,] <- Vec[ry + 1,]  - rem_vec  # year after remyear = 2 rows later filled with new stage vec
     Pop[ry + 2] <- sum(Vec[ry + 2]) # filling in total pop size
@@ -182,7 +230,8 @@ rem.proj <- function(Umat,   # MAX SURVIVAL
       Vec[(j + 1), ] <- thisAmat %*% Vec[j, ]  # following year stage vector is this Amat* this year pop structure - incorporate U here for max no. births?
       Pop[j + 1] <- sum(Vec[(j + 1), ])
     }
-  } 
+    }
+  
   # out objects
   out$pop <- Pop
   if(is.numeric(intensity)){   
@@ -199,6 +248,8 @@ rem.proj <- function(Umat,   # MAX SURVIVAL
   return(out)
 }
 
+
+
 # testing no removal scenario----
 proj0 <- rem.proj(Umat,      # seems to reach stability quickly - some kind of stochasticity needed?
                   initial = n0, 
@@ -210,6 +261,8 @@ proj0 <- rem.proj(Umat,      # seems to reach stability quickly - some kind of s
                   Mfunction= "Min",
                   intensity= NULL,  # percentage you want REMOVED from pop at time T=ry
                   remyear = NULL, 
+                  rem_strat = "random",  # if specified removals, "adults, females, yearlings, males, yearling females, 
+                  bias = NULL , # strength of bias as percentage (range??) how to ignore in function if null?
                   return.vec= TRUE, 
                   return.remvec = FALSE) 
 
@@ -230,7 +283,7 @@ N0_plot <- dd_plot(proj0,
                    theme = theme_classic(), 
                    cols= col_vec,    # can be vector of cols
                    legend.pos = "topright",
-                   cex.legend = 0.8)
+                   cex.legend = 0.8)  # issues plotting
 
 proj2 <- rem.proj(Umat,   # MAX SURVIVAL
                   initial = n0, 
@@ -242,6 +295,8 @@ proj2 <- rem.proj(Umat,   # MAX SURVIVAL
                   Mfunction= "Min",
                   intensity= 70,  # percentage you want REMOVED from pop at time t=ry
                   remyear = 10,  # removal year = decrease from following year
+                  rem_strat = "random",  # if specified removals, "adults, females, yearlings, males, yearling females, 
+                  bias = NULL , # strength of bias as percentage (range??) how to ignore in function if null?
                   return.vec= TRUE, 
                   return.remvec = TRUE) 
 
@@ -268,6 +323,8 @@ proj3 <- rem.proj(Umat,      # seems to reach stability quickly - some kind of s
                   Mfunction= "Min",
                   intensity= 90,  # percentage you want REMOVED from pop at time T=ry
                   remyear = 10, 
+                  rem_strat = "random",  # if specified removals, "adults, females, yearlings, males, yearling females, 
+                  bias = NULL , # strength of bias as percentage (range??) how to ignore in function if null?
                   return.vec= TRUE) 
 
 (proj3_plot <- dd_plot(proj3, 
@@ -311,7 +368,31 @@ ssd2_df <- as.data.frame(ssd2$stageMat)
 sapply(ssd2_df, mean, 1)
 
 
+# testing biased removals
+proj_bi <- rem.proj(Umat,      # seems to reach stability quickly - some kind of stochasticity needed?
+                  initial = n0, 
+                  params, 
+                  stagenames = stages, 
+                  time = 30, 
+                  memberN=NULL,  # which individuals contribute to pop size? (as vec)
+                  DDapply="Fmat", 
+                  Mfunction= "Min",
+                  intensity= 70,  # percentage you want REMOVED from pop at time T=ry
+                  remyear = 10, 
+                  rem_strat = "female",  # if specified removals, "adults, females, yearlings, males, yearling females, 
+                  bias = 0.2 , # strength of bias as percentage (range??) how to ignore in function if null?
+                  return.vec= TRUE, 
+                  return.remvec = TRUE) # not returned?
 
+(bias_plot <- dd_plot(proj_bi, 
+                       y_val= "Vec", 
+                       ylab = "Abundance", 
+                       xlab = "Time (t)",
+                       rem_year = 10,
+                       theme = theme_classic(), 
+                       cols= col_vec,    # can be vector of cols
+                       legend.pos = "topright",
+                       cex.legend = 0.8))
 
 
 
